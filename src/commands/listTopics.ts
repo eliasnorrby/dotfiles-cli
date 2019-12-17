c9415343f54079ca7dfd5caca3e803fd226791ec
Elias Norrby <elias.norrby@gmail.com>
2019-12-17 15:39:30 +0100

    refactor: add io/printTopicList

--- src/commands/listTopics.ts
+++ src/commands/listTopics.ts
@@ -1,16 +1,8 @@
 import Settings from "../settings/iSettings";
 import { readConfig } from "../io";
-import print from "../io/print";
+import printTopicList from "../io/printTopicList";
 
 export default function listTopics(settings: Settings, argv: any) {
   const topicGroups = readConfig(settings, argv);
-
-  const groups = Object.keys(topicGroups);
-
-  for (const g of groups) {
-    print.group(g);
-    for (const t of topicGroups[g]) {
-      print.topic(t);
-    }
-  }
+  printTopicList(topicGroups);
 }
