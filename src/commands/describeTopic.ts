a76ded25a2a622c63ec7a9fad08fe889e1cc19e0
Elias Norrby <elias.norrby@gmail.com>
2019-12-19 17:32:15 +0100

    feat: add syntax highlighting to describe output

--- src/commands/describeTopic.ts
+++ src/commands/describeTopic.ts
@@ -1,6 +1,7 @@
+import { highlight } from "cli-highlight";
 import yaml from "js-yaml";
-import Settings from "../settings/iSettings";
 import { readTopic, readTopicConfig } from "../io";
+import Settings from "../settings/iSettings";
 
 export default function describeTopic(
   settings: Settings,
@@ -11,5 +12,6 @@ export default function describeTopic(
 
   topic.config = readTopicConfig(settings, topicName);
   let yamlStr: string = yaml.safeDump({ [topicName]: topic });
-  console.log(yamlStr);
+
+  console.log(highlight(yamlStr, { language: "yaml", ignoreIllegals: true }));
 }
