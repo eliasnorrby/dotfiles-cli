1230a7149770a0bd0303a7f1183eea078882c388
Elias Norrby <elias.norrby@gmail.com>
2020-03-05 10:04:33 +0100

    feat: add the describeFlags util

---
+++ src/util/describeFlags.ts
@@ -0,0 +1,11 @@
+import { flagToTagMap } from "../constants/flagToTagMap"
+
+export const describeFlags = () => {
+  for (const entry of flagToTagMap) {
+    const { flag: operation, shortflag: shorthand, description } = entry
+    console.log(`operation: ${operation}`)
+    console.log(`shorthand: ${shorthand}`)
+    console.log(`description: ${description}`)
+    console.log('')
+  }
+}
